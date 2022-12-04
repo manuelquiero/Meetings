309c719b922268d60db54802f1436421d16cf553
Hanz Jonathan Manuel <hanzjmanuel@gmail.com>
2022-12-04 15:07:07 +0800

    Add project files.

---
+++ Meetings/Meeting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meetings
+{
+    public class Meeting
+    {
+        public DateTime StartDate { get; }
+        public DateTime EndDate { get; }
+    }
+}
